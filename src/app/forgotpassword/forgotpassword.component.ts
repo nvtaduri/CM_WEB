dd1754134fc14614f5d1288f746643867af6383b
srishivag <srishiva.abc@gmail.com>
2020-12-23 16:38:54 +0530

    changes in forgetpassword errors resolved

--- src/app/forgotpassword/forgotpassword.component.ts
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -32,52 +32,52 @@ export class ForgotpasswordComponent implements OnInit {
 
   user_id: any;
 
-  validateUser() {
-    if (this.username == '' || this.username == null || this.username == undefined) {
-      this.isInfo = true;
-    } else {
-      let userData = {
-        username: this.username
-      }
-      this.authService.validateUser(userData).subscribe(res => {
-        console.log("response is:", res);
-        if (res['success'] == true) {
-          this.user_id = res['data'][0].user_id;
-          console.log("userid is:", this.user_id);
-          this.isChange = false;
-          this.isSubmit = true;
-        } else if(res['statusCode'] == 404) {
-          this.isFalied = true;
-        } else {
-          this.isFalied = true;
-        }
-      })
-    }
-  }
+  // validateUser() {
+  //   if (this.username == '' || this.username == null || this.username == undefined) {
+  //     this.isInfo = true;
+  //   } else {
+  //     let userData = {
+  //       username: this.username
+  //     }
+  //     this.authService.validateUser(userData).subscribe(res => {
+  //       console.log("response is:", res);
+  //       if (res['success'] == true) {
+  //         this.user_id = res['data'][0].user_id;
+  //         console.log("userid is:", this.user_id);
+  //         this.isChange = false;
+  //         this.isSubmit = true;
+  //       } else if(res['statusCode'] == 404) {
+  //         this.isFalied = true;
+  //       } else {
+  //         this.isFalied = true;
+  //       }
+  //     })
+  //   }
+  // }
 
-  submitPassword() {
-    if ((this.password == '' || this.password == null || this.password == undefined) ||
-      (this.confirmpassword == '' || this.confirmpassword == null || this.confirmpassword == undefined)) {
-      this.isInfo = true;
-    } else if (this.password != this.confirmpassword) {
-      this.isWarning = true;
-    } else {
-      let userData = {
-        user_id: this.user_id,
-        password: this.password
-      }
-      this.authService.forgotPassword(userData).subscribe(res => {
-        console.log("response is:", res);
-        if (res['success'] == true) {
-          this.isSuccess = true;
-          setTimeout(()=>{
-            this.route.navigate(['']);
-          },1000)
-        } else {
-          this.isFalied = true;
-        }
-      })
-    }
-  }
+  // submitPassword() {
+  //   if ((this.password == '' || this.password == null || this.password == undefined) ||
+  //     (this.confirmpassword == '' || this.confirmpassword == null || this.confirmpassword == undefined)) {
+  //     this.isInfo = true;
+  //   } else if (this.password != this.confirmpassword) {
+  //     this.isWarning = true;
+  //   } else {
+  //     let userData = {
+  //       user_id: this.user_id,
+  //       password: this.password
+  //     }
+  //     this.authService.forgotPassword(userData).subscribe(res => {
+  //       console.log("response is:", res);
+  //       if (res['success'] == true) {
+  //         this.isSuccess = true;
+  //         setTimeout(()=>{
+  //           this.route.navigate(['']);
+  //         },1000)
+  //       } else {
+  //         this.isFalied = true;
+  //       }
+  //     })
+  //   }
+  // }
 
 }
