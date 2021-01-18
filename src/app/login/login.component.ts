e9301e50a4cdee9090c6cacc6cd88137a8e32bb8
srishivag <srishiva.abc@gmail.com>
2021-01-18 16:17:06 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,7 +42,7 @@ export class LoginComponent implements OnInit {
   }
 
   goToDashboard() {
-    this.route.navigate(['/dashboard']);
+    this.route.navigate(['/admin/dashboard']);
   }
 
   isSuccess: boolean = false;
@@ -51,54 +51,55 @@ export class LoginComponent implements OnInit {
   isBlock: boolean = false;
 
   userLogin() {
-    let userData = {
-      username: this.username,
-      password: this.password
-    }
-    console.log("userdata is:", userData);
-    this.authService.userLogin(userData).subscribe(res => {
-      console.log("response is:", res);
-      if (res['success'] == true) {
-        console.log("Login successful", res);
-        localStorage.setItem('token', res['token']);
-        sessionStorage.setItem('userid', res['data'][0].user_id);
-        sessionStorage.setItem('id', res['id']);
-        sessionStorage.setItem('role', res['role']);
-        sessionStorage.setItem('firstname', res['firstname']);
-        sessionStorage.setItem('lastname', res['lastname']);
-        sessionStorage.setItem('password', res['password']);
-        sessionStorage.setItem('phonenumber', res['phonenumber']);
-        sessionStorage.setItem('designation', res['designation']);
-        sessionStorage.setItem('department', res['department']);
-        sessionStorage.setItem('created_at', moment(res['created_at']).format('YYYY-MM-DD'));
-        this.isSuccess = true;
-        // this.sharedService.getModulesData();
-        setInterval(() => {
-          this.route.navigate([`/${res['role']}/dashboard`]);
-        }, 1000);
-      } else if (res['statusCode'] == 204) {
-        console.log("Required fields are empty");
-        this.isInfo = true;
-        setInterval(() => {
-          this.isInfo = false;
-        }, 5000);
-      } else if (res['statusCode'] == 404) {
-        console.log("Invalid username or password");
-        this.isFalied = true;
-        setInterval(() => {
-          this.isFalied = false;
-        }, 5000);
-      } else if (res['statusCode'] == 405) {
-        console.log("User is blocked");
-        this.isBlock = true;
-        setInterval(() => {
-          this.isBlock = false;
-        }, 5000);
-      }
-      else {
-        console.log('Login failed');
-      }
-    })
+    this.route.navigate(['/admin/dashboard']);
+    // let userData = {
+    //   username: this.username,
+    //   password: this.password
+    // }
+    // console.log("userdata is:", userData);
+    // this.authService.userLogin(userData).subscribe(res => {
+    //   console.log("response is:", res);
+    //   if (res['success'] == true) {
+    //     console.log("Login successful", res);
+    //     localStorage.setItem('token', res['token']);
+    //     sessionStorage.setItem('userid', res['data'][0].user_id);
+    //     sessionStorage.setItem('id', res['id']);
+    //     sessionStorage.setItem('role', res['role']);
+    //     sessionStorage.setItem('firstname', res['firstname']);
+    //     sessionStorage.setItem('lastname', res['lastname']);
+    //     sessionStorage.setItem('password', res['password']);
+    //     sessionStorage.setItem('phonenumber', res['phonenumber']);
+    //     sessionStorage.setItem('designation', res['designation']);
+    //     sessionStorage.setItem('department', res['department']);
+    //     sessionStorage.setItem('created_at', moment(res['created_at']).format('YYYY-MM-DD'));
+    //     this.isSuccess = true;
+    //     // this.sharedService.getModulesData();
+    //     setInterval(() => {
+    //       this.route.navigate([`/${res['role']}/dashboard`]);
+    //     }, 1000);
+    //   } else if (res['statusCode'] == 204) {
+    //     console.log("Required fields are empty");
+    //     this.isInfo = true;
+    //     setInterval(() => {
+    //       this.isInfo = false;
+    //     }, 5000);
+    //   } else if (res['statusCode'] == 404) {
+    //     console.log("Invalid username or password");
+    //     this.isFalied = true;
+    //     setInterval(() => {
+    //       this.isFalied = false;
+    //     }, 5000);
+    //   } else if (res['statusCode'] == 405) {
+    //     console.log("User is blocked");
+    //     this.isBlock = true;
+    //     setInterval(() => {
+    //       this.isBlock = false;
+    //     }, 5000);
+    //   }
+    //   else {
+    //     console.log('Login failed');
+    //   }
+    // })
   }
 
 }
