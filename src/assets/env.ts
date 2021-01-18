e547b5f25ca478d9e463e2eb6894b6ee2a0dddfe
srishivag <srishiva.abc@gmail.com>
2021-01-18 16:08:42 +0530

    env changes

--- src/assets/env.ts
+++ src/assets/env.ts
@@ -1,4 +1,5 @@
 export const serverIP: any = {
-    apiUrl: 'http://localhost:1000',
+    apiUrl: 'http://http://192.168.1.59:4850',
+    //apiUrl: 'http://localhost:1000',
     // imgUrl: 'http://localhost:3333/'
 }
